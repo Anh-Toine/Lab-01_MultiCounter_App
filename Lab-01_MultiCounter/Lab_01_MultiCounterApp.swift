1a61f05c1afd302d85034c64772e50f0f0289ec6
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-09-02 13:31:14 -0400

    Initial Commit

---
+++ Lab-01_MultiCounter/Lab_01_MultiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_01_MultiCounterApp.swift
+//  Lab-01_MultiCounter
+//
+//  Created by Lukas Nguyen on 2022-09-02.
+//
+
+import SwiftUI
+
+@main
+struct Lab_01_MultiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
